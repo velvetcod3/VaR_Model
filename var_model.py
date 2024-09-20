d6fb00d3c4b2fd53e9eefe18612554feedfcd3ec
Khandi <khandinicholas@gmail.com>
2024-09-20 18:56:49 +0100

    applied the criteria filters

--- var_model.py
+++ var_model.py
@@ -7,7 +7,16 @@ import matplotlib.pyplot as plt
 
 tickers = ['MSFT', '0700.HK', 'ABBV', 'MRK', 'NESN.SW', 'ULVR.L', 'DBK.DE', 'C', 'SHEL.L', '8058.T', 'CAT', 'MELI', 'JMIA']
 
+# Tickers based on the following criteria filters 
+max_beta = 1.5 
+max_sector_weight = 0.35 
+max_pe_ratio = 30
+min_dividend_yield = 0.03 
+
 market_caps = {}
+filtered_stocks = []
+sector_weights = {}
+
 # Download market capitalization or historical price data for the tickers
 for ticker in tickers:
     stock = yf.Ticker(ticker)
