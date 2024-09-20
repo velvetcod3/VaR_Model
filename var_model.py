de6f070bc21b440274d67f79b11531404d9dcbc4
Khandi <khandinicholas@gmail.com>
2024-09-20 17:37:18 +0100

    Applied stock filter criteria for portfolio and added subtraction function

---
+++ var_model.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+# Choice of 13 stock tickers selected
+
+tickers = ['MSFT', '0700.HK', 'ABBV', 'MRK', 'NESN.SW', 'ULVR.L', 'DBK.DE', 'C', 'SHEL.L', '8058.T', 'CAT', 'MELI', 'JMIA']
+
+market_caps = {}
+# Download market capitalization or historical price data for the tickers
+for ticker in tickers:
+    stock = yf.Ticker(ticker)
+    
+    # Fetch the market cap and store it in the market_caps dictionary
+    market_data = stock.info
+    market_caps[ticker] = market_data.get('marketCap', None)
+    
+# Display market caps
+print(market_caps)
+
+# Calculate total market capitalization
+total_market_cap = sum(market_caps.values())
+
+# Calculate the weights based on market capitalization
+weights = {ticker: market_cap / total_market_cap for ticker, market_cap in market_caps.items()}
+
+# Display portfolio weights
+print(weights)
